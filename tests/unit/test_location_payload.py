fe3c47593f8c1c6105e215f4e723dbe37764c173
Niccolò Malenotti <malenottinicco@gmail.com>
2026-02-13 13:09:28 +0100

    Handle MAP_REPORT_APP when MapReport proto is unavailable

--- tests/unit/test_location_payload.py
+++ tests/unit/test_location_payload.py
@@ -3,19 +3,6 @@
 from malla.utils import location_payload
 
 
-class _FakeMapReportMissingCoords:
-    """MapReport variant that parses successfully but has no coordinates."""
-
-    def __init__(self):
-        self.latitude_i = 0
-        self.longitude_i = 0
-        self.altitude = 0
-        self.position_precision = None
-
-    def ParseFromString(self, _: bytes) -> None:  # noqa: N802
-        return
-
-
 class _FakePositionWithCoords:
     """Legacy Position payload carrying coordinates for MAP_REPORT_APP."""
 
@@ -29,9 +16,17 @@ class _FakePositionWithCoords:
         return
 
 
-class _FakeMeshPb2Fallback:
-    MapReport = _FakeMapReportMissingCoords
-    Position = _FakePositionWithCoords
+class _FakeMapReportMissingCoords:
+    """MapReport variant that parses but has no coordinates."""
+
+    def __init__(self):
+        self.latitude_i = 0
+        self.longitude_i = 0
+        self.altitude = 0
+        self.position_precision = None
+
+    def ParseFromString(self, _: bytes) -> None:  # noqa: N802
+        return
 
 
 class _FakeMapReportWithCoords:
@@ -45,20 +40,31 @@ class _FakeMapReportWithCoords:
         return
 
 
-class _FakePositionUnused:
-    def __init__(self):
-        self.latitude_i = 0
-        self.longitude_i = 0
-        self.altitude = 0
-        self.precision_bits = None
+class _FakeMeshPb2WithoutMapReport:
+    Position = _FakePositionWithCoords
 
-    def ParseFromString(self, _: bytes) -> None:  # noqa: N802
-        return
+
+class _FakeMeshPb2Fallback:
+    MapReport = _FakeMapReportMissingCoords
+    Position = _FakePositionWithCoords
 
 
 class _FakeMeshPb2Native:
     MapReport = _FakeMapReportWithCoords
-    Position = _FakePositionUnused
+    Position = _FakePositionWithCoords
+
+
+def test_decode_map_report_works_when_mapreport_class_is_missing(monkeypatch):
+    monkeypatch.setattr(location_payload, "mesh_pb2", _FakeMeshPb2WithoutMapReport)
+
+    decoded = location_payload.decode_position_from_raw_payload(73, b"dummy")
+
+    assert decoded == {
+        "latitude": 40.9075712,
+        "longitude": 14.7718144,
+        "altitude": 442,
+        "precision": 17,
+    }
 
 
 def test_decode_map_report_falls_back_to_legacy_position_when_mapreport_has_no_coords(
